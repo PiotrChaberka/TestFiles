4aa6de6a0944d03f12e19d5ffb48026f0398d6d8
EvixWasTaken <piotr5wwwwwz@gmail.com>
2025-08-17 11:06:11 +0200

    t1

--- Test/Untitled-1.py
+++ Test/Untitled-1.py
@@ -1,6 +1,13 @@
-def rekurencja(a):
-    print("rekurencja")
-    if a > 0:
-        rekurencja(a - 1)
+with open(r'C:\Users\piotr\Pulpit\PythonPrograms\test\TestFiles\Test\data_test.txt', 'r') as file:
+    data = file.read()
 
-rekurencja(5)
+
+
+split_data = data.splitlines()
+print(split_data)
+
+for i in range(len(split_data)):
+    if int(split_data[i]) > 100:
+        print("over 100", split_data[i])
+    else:
+        print("less than 100", split_data[i])
